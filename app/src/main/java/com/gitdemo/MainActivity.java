aac0e1da7f4dd304748d7b2ea0dc9cefa0b7b8d5
Moseqi <moseqi7990@gmail.com>
2018-02-28 16:33:30 +0530

    kl

--- app/src/main/java/com/gitdemo/MainActivity.java
+++ app/src/main/java/com/gitdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gitdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GPSTracker gpsTracker = new GPSTracker(this);
+        Log.d("gpsTrackerLog","gpsTracker");
     }
 }
